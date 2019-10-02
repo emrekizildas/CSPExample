b660a8f2ac2687e7d836d2eb43ab80d838866cdb
emrekizildas <kizildas@icloud.com>
2019-10-02 22:50:22 +0300

    * .gitignore: First commit.
    * CSPExample.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * CSPExample.csproj:
    * site.js:
    * favicon.ico:
    * site.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Privacy.cshtml:
    * Error.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * bootstrap-grid.css.map:
    * bootstrap-grid.min.css:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ CSPExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSPExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
